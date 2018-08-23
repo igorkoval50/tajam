ad813cc054b88539247c2fbe3684fc4a86cfad30
igor <ihor.koval.91@mail.ru>
2018-08-23 15:37:11 +0400

    created section header

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,26 @@ import DE from './modules/dots';
   });
 
 })(jQuery);
+
+//Responsive menu for header 
+
+$(function() {
+	let link = $(".menu-link");
+	let linkActive = $(".menu-link_active");
+	let menu = $(".menu");
+	let nav = $(".menu a");
+	let body = $('html, body');
+	
+	link.on("click", function() {
+		link.toggleClass('menu-link_active');
+		menu.toggleClass('menu-active');
+		body.toggleClass('act');
+	});
+	linkActive.on("click", function() {
+		link.removeClass('menu-link_active');
+	});
+	nav.on("click", function() {
+		menu.toggleClass('menu-active');
+		link.toggleClass('menu-link_active');
+	});
+});
